d37ebae6f7d28c10b0e31bf019223ee6581fd5de
vjraj <explorevijayraj@gmail.com>
2017-03-24 23:31:42 +0530

    Update & add xenonhd.dependencies
    Change-Id: I8cd1f651fcd263f4baeebd9678f207b30dd557bc

--- xenonhd.mk
+++ xenonhd.mk
@@ -21,12 +21,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/armani/device.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/armani/armani-vendor.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common files
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
+
+WITH_SUPERSU := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := armani
-PRODUCT_NAME := lineage_armani
+PRODUCT_NAME := xenonhd_armani
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := HM 1S
 PRODUCT_MANUFACTURER := Xiaomi
@@ -35,3 +37,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=armani
+
+PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer="VjRaj"
